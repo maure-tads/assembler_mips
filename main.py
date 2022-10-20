7afb37c1c0a7755fc68231d5a789e0ab037c8b83
Pedro Maure Frutuoso de Andrade <20221014040013@ifrn.local>
2022-10-20 11:49:36 -0300

    falta só converter pra hex

--- main.py
+++ main.py
@@ -7,42 +7,61 @@ class Assembler:
 		self.instructions_set = json.load(open(instructions_set))
 		
 	def load_program(self, program_path):
-		self.program = open(program_path, 'r').read().split('\n')
+		self.program = re.sub(' +', ' ', open(program_path, 'r').read().strip().replace("  ", " ")).split('\n')
 
 	def get_values_of(self, c):
 		return c['arguments'], c['word'], c['immediate']
-	
-	def convert_parameters_values(self, size, line):
-		for i in range(1, size):
 
-			
+	def convert_number_to_binary(self, n_str, size):
+		return ("{0:0" + str(size) + "b}").format(int(n_str))
 	
+	def convert_parameters_values(self, line, size, bits):
+		for i in range(size):
+			x = re.search("[0-9]+", line[i])
+			line[i] = self.convert_number_to_binary(x.group(0) if x else line, bits)
+		return line
+			
+	def toHex(self, word):
+		i = 0
+		while i < len(word):
+			print(word[i:i+4])
+			i += 4
+
+
 	def process_line(self, line):
 		line = line.split(' '); 
 		if line[0] not in self.instructions_set:
-			return;
+			return
+		command = self.instructions_set[line[0]]
+		
 
-		self.convert_parameters_values(line)
-
-		argc, word, immediate = self.get_values_of(self.instructions_set[line[0]])
-		# if argc == 3:
-		# 	if 'a' in word:
-		# 		print("op $1 $2 $3")
-		# 	else:
-		# 		word = word.replace('s', line[2])
-		# 		word = word.replace('t', line[3])
-		# 		word = word.replace('d', line[1])
-		# 		print(word)
-		# elif argc == 2:
-		# 	if immediate:
-		# 		print("opi $1 $2 10")
-		# 	else:
-		# 		print("op $1 $2")
+		argc, word, immediate = self.get_values_of(command)
+		if argc == 3:
+			line = self.convert_parameters_values(line[1:argc+1], 3, 5)
+			if 'a' in word:
+				word = word.replace('t', line[1])
+				word = word.replace('d', line[0])
+				word = word.replace('a', line[2])
+			else:
+				word = word.replace('s', line[1])
+				word = word.replace('t', line[2])
+				word = word.replace('d', line[0])
+		elif argc == 2:
+			if immediate:
+				line = self.convert_parameters_values(line[1:4], 2, 5)
+				word = word.replace('s', line[1])
+				word = word.replace('t', line[0])
+				word = word.replace('*', self.convert_number_to_binary(line[2], 16))
+			else:
+				line = self.convert_parameters_values(line[1:3], 2, 5)
+				word = word.replace('s', line[0])
+				word = word.replace('t', line[1])
+		return word
 
 	def parse(self, program_path):
 		self.load_program(program_path)
 		for line in self.program:
-			self.process_line(line)
+			self.toHex(self.process_line(line))
 
 	def print_instructions_set(self):
 		for i in self.instructions_set:
